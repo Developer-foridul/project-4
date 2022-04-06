5465b5c4bafa085d0cf344d6aa6b670a55640007
Md.Foridul islam <103020129+Developer-foridul@users.noreply.github.com>
2022-04-06 13:01:28 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+//I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
+
+//For creating a product upload form what specifications will be uploaded and what will be their data types?//
+
+//Directions://
+
+$Name = " (Foridul) ";// string
+$Age = 21.5;//float/ double
+$class = 14; //integer
+$WebsiteDesign = true; //boolean
+$EearnMony = null; //null
+
+ echo  "My Name is " . gettype($Name) . $Name ."<br>";
+ echo " My Age " . gettype($Age) .  $Age  ."<br>";
+ echo gettype($WebsiteDesign) . $WebsiteDesign . "Yes,I'm website Designer ";
+
+
+?>
